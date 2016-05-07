8272bb0e64d26ed4e9becabc57a1475e16120f1f
Conrad G Bailey <grhmbailey@gmail.com>
2016-05-07 15:46:49 -0600

    it works kind of

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,9 +1,12 @@
 angular.module("movieApp")
-.controller("MovieController", ['$scope', '$routeParams', 'mainService', function($scope, $routeParams, mainService){
+.controller("MovieController", ['$scope', '$routeParams', '$http', 'mainService', function($scope, $routeParams, $http, mainService){
+        console.log("controller")
+        $scope.view = {};
         $scope.view.movie = {};
-        console.log($routeParams)
+        console.log($routeParams.id);
           $http.get("http://www.omdbapi.com/?i="+$routeParams.id).then(function(response) {
             $scope.view.movie = response.data;
+            console.log($scope.view.movie);
           })
 
 }])
--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,9 +1,10 @@
 angular.module("movieApp")
-.controller("SearchController", ['$scope', '$routeParams', 'mainService', "$http", function($scope, $routeParams, mainService, $http){
+.controller("SearchController", ['$scope', '$routeParams', 'mainService', '$http', '$location', function($scope, $routeParams, mainService, $http, $location){
   $scope.view = {};
-  console.log($routeParams)
     $http.get("http://www.omdbapi.com/?s="+$routeParams.title).then(function(response) {
-      console.log(response.data)
       $scope.view.movies = response.data.Search;
     })
+  $scope.view.singleMovie = function(id){
+    $location.path('/movies/' + id);
+  }
 }])
